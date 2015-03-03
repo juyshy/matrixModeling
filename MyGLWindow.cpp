80b78e299c2d4b38429e72c9c2891eb5e7d83a1c
Jukka Ylitalo <juylitalo@gmail.com>
2015-03-03 21:34:13 +0200

    Element Array Buffers

--- MyGLWindow.cpp
+++ MyGLWindow.cpp
@@ -8,8 +8,6 @@ void MyGLWindow::initializeGL(){
 		 0.0f,   0.0f,
 		1.0f,   1.0f,
 		 -1.0f,  1.0f,
-
-		 0.0f, 0.0f,
 		 -1.0f, -1.0f,
 		 1.0f, -1.0f,
 	};
@@ -20,13 +18,20 @@ void MyGLWindow::initializeGL(){
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+	GLushort indices[] = { 0, 1, 2, 0, 3, 4 };
+	GLuint indexBufferID;
+	glGenBuffers(1, &indexBufferID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 }
 
 void MyGLWindow::paintGL(){
 	glClearColor(0.5, 0, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glViewport(0, 0, width(), height());
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	//glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
 
 MyGLWindow::MyGLWindow()
