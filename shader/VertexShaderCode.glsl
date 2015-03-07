1e110c5168c804c83bf9d247577bf20863c98b49
Jukka Ylitalo <juylitalo@gmail.com>
2015-03-07 14:58:47 +0200

    .

--- shader/VertexShaderCode.glsl
+++ shader/VertexShaderCode.glsl
@@ -4,18 +4,14 @@ in layout(location=0) vec3 position;
 in layout(location=1) vec3 vertexColor;
 in layout(location=2) vec3 normal;
 
-uniform vec3 ambientLight;
-uniform vec3 lightPosition;
-uniform mat4 fullTransformMatrix;
-
+ 
 out vec3 theColor;
 
 void main()
 {
  vec4 v = vec4(position,  1.0);
  
- gl_Position =  v ;// fullTransformMatrix *
- vec3 lightVector = normalize(lightPosition-position);
- //float brightness =  dot(lightVector, normal);// vertexColor *ambientLight;
- theColor =  vertexColor;// * dot(lightVector, normal);// vec3(brightness,brightness,brightness); // vertexColor * dot(lightVector, normal);
+ gl_Position =  v ; 
+   
+ theColor =  vertexColor; 
 }
